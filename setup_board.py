241b6b7a0c280263f69882b758d870e222bb75ac
ejobe <ejo@uchicago.edu>
2021-03-25 22:46:31 +0000

    update board setup. working bit shifting

--- setup_board.py
+++ setup_board.py
@@ -16,7 +16,10 @@ HMCAD_ADR_LVDS_DRIVE = 0x11
 HMCAD_ADR_TP_MODE = 0x25
 HMCAD_ADR_TP_WORD1 = 0x26
 HMCAD_ADR_TP_WORD2 = 0x27
+HMCAD_ADR_TP_SYNC = 0x45
 HMCAD_ADR_PHASE_DDR = 0x42
+HMCAD_ADR_BYTE_FORMAT = 0x46
+HMCAD_ADR_STARTUP = 0x56
 
 def spiWriteBothADCS(dev, reg, cmd_hi, cmd_lo):
     dev.write(dev.DEV_FLOWER, [CONFIG_REG_0, 0,0,0]) #select ADC0
@@ -38,35 +41,46 @@ def datValid(dev, en):
         dev.write(dev.DEV_FLOWER, [dev.map['DATVALID'], 0, 1, 0])
     else:
         dev.write(dev.DEV_FLOWER, [dev.map['DATVALID'], 0, 0, 0])
-    
+
 if __name__=='__main__':
 
     ## configure PLL
-    pll = pll_config.ClockConfig()
-    pll.configure('config/Si5338-RevB-Registers-472MHz.h')
+    #pll = pll_config.ClockConfig()
+    #pll.configure('config/Si5338-RevB-Registers-472MHz.h')
 
     ## setup ADC chips
-    
     dev = flower.Flower()
     datValid(dev, 0)
 
-    #ADC configurations:
-    #adcReset(dev)
-    spiWriteBothADCS(dev, HMCAD_ADR_NUM_CHAN, 0, 2) #config to 2-chan operation, clkdivide=1
-    spiWriteBothADCS(dev, HMCAD_ADR_INPUTSEL_0, 0x02, 0x02) #set IN1 to ADC1 and ADC2
+    ## ADC configuration
+    spiWriteBothADCS(dev, HMCAD_ADR_NUM_CHAN, 0, 2) #config to 2-chan operation, clkdivide=1    
+    spiWriteBothADCS(dev, HMCAD_ADR_STARTUP, 0, 4) #configure start-up time according to datasheet
+    spiWriteBothADCS(dev, HMCAD_ADR_INPUTSEL_0, 0x02, 0x02) #set IN1 to ADC1 and ADC2    
     spiWriteBothADCS(dev, HMCAD_ADR_INPUTSEL_1, 0x80, 0x80) #set IN3 to ADC3 and ADC4
     spiWriteBothADCS(dev, HMCAD_ADR_LVDS_DRIVE, 0x05, 0x55) #set LVDS drive to RSDS standards
-
-    
-
-    spiWriteBothADCS(dev, HMCAD_ADR_TP_WORD1, 0x0E, 0x00)
-    spiWriteBothADCS(dev, HMCAD_ADR_TP_WORD2, 0x1E, 0x00)
+    spiWriteBothADCS(dev, HMCAD_ADR_BYTE_FORMAT, 0x00, 0x08) #set MSB first
     spiWriteBothADCS(dev, HMCAD_ADR_PHASE_DDR, 0x00, 0x60) #set DDR phase to 0
+    #spiWriteBothADCS(dev, HMCAD_ADR_PHASE_DDR, 0x00, 0x40) #set DDR phase to 90
+    #spiWriteBothADCS(dev, HMCAD_ADR_PHASE_DDR, 0x00, 0x20) #set DDR phase to 180
+    #spiWriteBothADCS(dev, HMCAD_ADR_PHASE_DDR, 0x00, 0x00) #set DDR phase to 270 <-- not stable !?
+    
+    # RESET:
+    adcReset(dev)
+
+    # Test Pattern 
+    spiWriteBothADCS(dev, HMCAD_ADR_TP_WORD1, 0xAF, 0x00)
+    spiWriteBothADCS(dev, HMCAD_ADR_TP_WORD2, 0x70, 0x00)
 
     #spiWriteBothADCS(dev, HMCAD_ADR_TP_MODE, 0x00, 0x40) #set ramp test pattern
     spiWriteBothADCS(dev, HMCAD_ADR_TP_MODE, 0x00, 0x20) #set dual-custom word pattern
     #spiWriteBothADCS(dev, HMCAD_ADR_TP_MODE, 0x00, 0x10) #set single-custom word pattern
     #spiWriteBothADCS(dev, HMCAD_ADR_TP_MODE, 0x00, 0x00) #turn off test pattern
+    #spiWriteBothADCS(dev, HMCAD_ADR_TP_SYNC, 0x00, 0x02) #enable sync pattern
+    spiWriteBothADCS(dev, HMCAD_ADR_TP_SYNC, 0x00, 0x00) #turn off sync pattern
+
+    dev.write(dev.DEV_FLOWER, [0x42, 0x00, 0x00, 0x00])
+
+    adcReset(dev)
     
     ##enable data-valid --> data from ADCs should start flowing into FPGA
     datValid(dev, 1)
