ff2e66d1baaa57ebd533fe9aeb6538df44b759fb
ejobe <ejo@uchicago.edu>
2021-06-06 15:35:10 -0500

    update alignment

--- align_adcs.py
+++ align_adcs.py
@@ -1,6 +1,7 @@
 import flower
 import time
 
+ADC0_SAMPLE_SHIFT_REG = 0x38
 ADC1_SAMPLE_SHIFT_REG = 0x39
 
 def acquire(dev):
@@ -21,7 +22,7 @@ def getPeaks(dat):
     else: return peak0, peak1
 
 def align(dev, num_tries=20):
-    
+    dev.write(dev.DEV_FLOWER, [ADC0_SAMPLE_SHIFT_REG, 0, 0, 0x00]) #start with no adjustment
     dev.write(dev.DEV_FLOWER, [ADC1_SAMPLE_SHIFT_REG, 0, 0, 0x00]) #start with no adjustment
     tries=0
     while(tries < num_tries):
@@ -40,7 +41,23 @@ def align(dev, num_tries=20):
                 dev.write(dev.DEV_FLOWER, [ADC1_SAMPLE_SHIFT_REG, 0, 0, 0x05])
             elif diff ==-2: #adc1 faster by 2 samples    
                 dev.write(dev.DEV_FLOWER, [ADC1_SAMPLE_SHIFT_REG, 0, 0, 0x06])
-            else: return 1 #exceeds +/- 2 sample diff.
+            elif diff == 3:
+                #speed up adc1 by 2, slow adc0 by 1
+                dev.write(dev.DEV_FLOWER, [ADC1_SAMPLE_SHIFT_REG, 0, 0, 0x02])
+                dev.write(dev.DEV_FLOWER, [ADC0_SAMPLE_SHIFT_REG, 0, 0, 0x05])
+            elif diff == 4:
+                #speed up adc1 by 2, slow adc0 by 2
+                dev.write(dev.DEV_FLOWER, [ADC1_SAMPLE_SHIFT_REG, 0, 0, 0x02])
+                dev.write(dev.DEV_FLOWER, [ADC0_SAMPLE_SHIFT_REG, 0, 0, 0x06])
+            elif diff == -3:
+                #slow adc1 by 2, speed adc0 by 1
+                dev.write(dev.DEV_FLOWER, [ADC1_SAMPLE_SHIFT_REG, 0, 0, 0x06])
+                dev.write(dev.DEV_FLOWER, [ADC0_SAMPLE_SHIFT_REG, 0, 0, 0x01])
+            elif diff == -4:
+                #speed up adc1 by 2, slow adc0 by 2
+                dev.write(dev.DEV_FLOWER, [ADC1_SAMPLE_SHIFT_REG, 0, 0, 0x06])
+                dev.write(dev.DEV_FLOWER, [ADC0_SAMPLE_SHIFT_REG, 0, 0, 0x02])
+            else: return 1 #exceeds +/- 4 sample diff.
 
     return 2 #timeout return
             
