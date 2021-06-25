b71dd6b92a0d4f4b720fca72305e080ab67a526e
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2021-06-25 15:22:19 +0000

    small updates

--- simple_acquire.py
+++ simple_acquire.py
@@ -5,13 +5,19 @@ if __name__ == '__main__':
 
     
     dev = flower.Flower()
-    setup.adcGainSelect(dev,5)
+    setup.adcGainSelect(dev,0)
 
-    dev.calPulser(False)
+    dev.calPulser(True)
 
-    dev.softwareTrigger(0)
-    dev.softwareTrigger(1)
-    dat= dev.readRam(dev.DEV_FLOWER, 0, 256)
-    dev.softwareTrigger(0)
+    for i in range(100):
+        dev.softwareTrigger(0)
+        dev.softwareTrigger(1)
+        dat= dev.readRam(dev.DEV_FLOWER, 0, 256)
+        testdat = numpy.array(dat)
+        dev.softwareTrigger(0)
+        if numpy.min(testdat) < 80:
+              print ( 'bad' )
+              break
 
     numpy.savetxt('test.txt', numpy.array(dat))
+    dev.calPulser(False)
--- simple_trigger_test.py
+++ simple_trigger_test.py
@@ -3,16 +3,17 @@ import time
 
 trig = flower_trig.FlowerTrig()
 trig.dev.calPulser(True)
-
-for i in range(20, 1, -1):
+effective_disable_thresh = [120,120,120,120]
+for i in range(40, 1, -1):
     print (i)
-    trig.initCoincTrig(1, [40,i,i,i], vppmode=0)
+    trig.initCoincTrig(3, [i,40,i,i], [i,i,i,i], vppmode=0)
     print (trig.dev.readRegister(trig.dev.DEV_FLOWER, 0x57))
     time.sleep(1)
-    for j in range(12):
+    for j in range(18):
         trig.setScalerOut(j)
         print (trig.readSingleScaler())
+    trig.setScalerOut(31)
+    print ('scaler pps', trig.readSingleScaler())
 
-
-
-trig.initCoincTrig(3, [0,0,0,0], vppmode=0)
+trig.dev.calPulser(False)
+trig.initCoincTrig(3, effective_disable_thresh, effective_disable_thresh, vppmode=0)
