d08660238b516e5775db0a86d6f6a30136a76cfb
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2021-06-25 18:48:50 +0000

    try to fix merge issues

--- simple_trigger_test.py
+++ simple_trigger_test.py
@@ -3,20 +3,13 @@ import time
 import setup_board
 
 trig = flower_trig.FlowerTrig()
-<<<<<<< HEAD
+
 trig.dev.calPulser(False)
 setup_board.adcGainSelect(trig.dev, 5)
 effective_disable_thresh = [120,120,120,120]
 for i in range(20, 1, -1):
     print (i)
     trig.initCoincTrig(3, [i,40,i,i], [i,i,i,i], vppmode=1)
-=======
-trig.dev.calPulser(True)
-effective_disable_thresh = [120,120,120,120]
-for i in range(40, 1, -1):
-    print (i)
-    trig.initCoincTrig(3, [i,40,i,i], [i,i,i,i], vppmode=0)
->>>>>>> b71dd6b92a0d4f4b720fca72305e080ab67a526e
     print (trig.dev.readRegister(trig.dev.DEV_FLOWER, 0x57))
     time.sleep(1)
     for j in range(18):
