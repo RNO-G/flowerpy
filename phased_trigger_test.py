20fa42c27a832cd4f664c26bb743d437f3f49ca2
Ryan Krebs <ryankrebs016@gmail.com>
2025-04-22 12:36:12 -0400

    change vals

--- phased_trigger_test.py
+++ phased_trigger_test.py
@@ -7,11 +7,11 @@ trig = flower_trig.FlowerTrig()
 #trig.dev.calPulser(True,freq=1)
 #trig.dev.calPulser(False)
 #setup_board.adcGainSelect(trig.dev, 0)
-trig.initPhasedTrig(power=0xfff,servo=0xfff,mask=0x100,offset=0)
+trig.initPhasedTrig(power=0xfff,servo=0xfff,mask=0xfff,offset=0)
 trig.trigEnable(phased_trig=1)
-for i in range(2000, 0, -50):
+for i in range(2000, 500, -20):
     print(i)
-    trig.initPhasedTrig(power=i,offset=0,mask=0x100)
+    trig.initPhasedTrig(power=i,offset=0,mask=0xfff)
     print (trig.dev.readRegister(trig.dev.DEV_FLOWER, 0x57))
     time.sleep(1)
     for j in range(10,30,1):
